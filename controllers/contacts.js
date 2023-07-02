34fa9898147631a4b62610fded4db504128fe0ab
Danylo Bay <danylobay@gmail.com>
2023-07-02 16:14:05 +0200

    03-mongoose, mongodb
    created separate folders for controllers and routes, added controller Wrapper for operation functions, written middlewares for cheking is valid id

---
+++ controllers/contacts.js
@@ -0,0 +1,67 @@
+const { Contact } = require("../models/contact");
+const { ctrlWrapper, errorHandler } = require("../helpers");
+
+const getAll = async (req, res, next) => {
+  try {
+    const result = await Contact.find();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getById = async (req, res, next) => {
+  const { contactId } = req.params;
+  // const result = await Contact.findOne({ _id: contactId });
+  const result = await Contact.findById(contactId);
+
+  if (!result) {
+    throw errorHandler(404, "Not Found");
+  }
+  res.json(result);
+};
+
+const add = async (req, res, next) => {
+  const result = await Contact.create(req.body);
+  return res.status(201).json(result);
+};
+
+const deleteById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+  if (!result) {
+    throw errorHandler(404, "Not Found");
+  }
+  res.status(200).json({ message: "contact deleted" });
+};
+
+const updateById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw errorHandler(404, "Not Found");
+  }
+  return res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw errorHandler(404, "Not Found");
+  }
+  return res.json(result);
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  deleteById: ctrlWrapper(deleteById),
+  updateById: ctrlWrapper(updateById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+};
---
+++ helpers/index.js
@@ -0,0 +1,9 @@
+const handleMongooseError = require("./handleMongooseError");
+const ctrlWrapper = require("./ctrlWrapper");
+const errorHandler = require("./errorHandler");
+
+module.exports = {
+  handleMongooseError,
+  ctrlWrapper,
+  errorHandler,
+};
