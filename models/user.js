bd113ea1675e17ed6dcc6861e30577aa2427303e
Danylo Bay <danylobay@gmail.com>
2023-07-09 17:19:59 +0200

    add avatar
    avatar

--- models/user.js
+++ models/user.js
@@ -28,6 +28,9 @@ const userSchema = new Schema(
       enum: subscriptionList,
       default: "starter",
     },
+    avatarURL: {
+      type: String,
+    },
     token: String,
   },
   { versionKey: false, timestamps: true }
